9e7d25ca0d120c39ae98a8aeaf9e790f6c2ea4e7
Jeremy Bauer <48760451+cablebauer@users.noreply.github.com>
2023-02-20 09:12:44 -0600

    Jeremy Changes Sunday

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -24,32 +25,8 @@ import frc.robot.subsystems.*;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final XboxController primaryDriver = new XboxController(0);
-
-  /* Joystick and Controller assignments */
-  /* Drive Controls */
-  private final int translationAxis = XboxController.Axis.kLeftY.value;
-  private final int strafeAxis = XboxController.Axis.kLeftX.value;
-  private final int rotationAxis = XboxController.Axis.kRightX.value;
-
-  //
-  /* Driver Buttons - Xbox Controller */
-  // zero gyro
-  private final JoystickButton zeroGyro = new JoystickButton(primaryDriver, XboxController.Button.kY.value);
-  // test binding to get GyroPosition
-  private final JoystickButton getGyroReading = new JoystickButton(primaryDriver, XboxController.Button.kX.value);
-  // Set to field drive - Robot will default to this upon startup
-  private final JoystickButton FieldDrive = new JoystickButton(primaryDriver, XboxController.Button.kLeftBumper.value);
-  // Set to Robot Centric
-  private final JoystickButton robotCentric = new JoystickButton(primaryDriver,
-      XboxController.Button.kRightBumper.value);
-
-  /* Co-Driver Buttons - Dual Joysticks */
-
-  /* End of Joystick and Controller assignments */
-
-  /* Subsystems */
-  private final SwerveDrive s_Swerve = new SwerveDrive();
-
+  private final Joystick CoDriverJ1 = new Joystick(1);
+  private final Joystick CoDriverJ2 = new Joystick(2);
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
    */
@@ -66,7 +43,36 @@ public class RobotContainer {
     configureButtonBindings();
   }
 
-  /**
+   /* Subsystems */
+   private final SwerveDrive s_Swerve = new SwerveDrive();
+  
+   /* End Subsystems */
+
+  /* Joystick and Controller assignments */
+  /* Driver Buttons - Xbox Controller */
+  /* Analog Sticks */
+  private final int translationAxis = XboxController.Axis.kLeftY.value;
+  private final int strafeAxis = XboxController.Axis.kLeftX.value;
+  private final int rotationAxis = XboxController.Axis.kRightX.value;
+  // button assignments
+  private final JoystickButton getGyroReading = new JoystickButton(primaryDriver, XboxController.Button.kX.value); // get GyroPosition
+  private final JoystickButton zeroGyro = new JoystickButton(primaryDriver, XboxController.Button.kY.value);  // reset GyroPosition
+  private final JoystickButton FieldDrive = new JoystickButton(primaryDriver, XboxController.Button.kLeftBumper.value); // Set to Field Centric
+  private final JoystickButton robotCentric = new JoystickButton(primaryDriver, XboxController.Button.kRightBumper.value);   // Set to Robot Centric
+  /* Co-Driver Buttons - Dual Joysticks */
+  private final int OuterArmAxis = Joystick.AxisType.kY.value;
+  private final int InnerArmAxis = Joystick.AxisType.kY.value;
+//TODO dual joystick commands to be entered here
+ 
+  /* Button Bindings - Actions taken upon button press or hold */
+  private void configureButtonBindings() { 
+    zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroGyro())); //Y
+    getGyroReading.onTrue(new InstantCommand(() -> s_Swerve.GetGyroReading()));  //X
+    robotCentric.onTrue(new InstantCommand(() -> s_Swerve.SetRobotCentric()));  //LEFT BUMPER
+    FieldDrive.onTrue(new InstantCommand(() -> s_Swerve.SetFieldDrive()));  //RIGHT BUMPER
+    
+  }
+   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
    *
    * @return the command to run in autonomous
@@ -76,14 +82,5 @@ public class RobotContainer {
     // return new exampleAuto(s_Swerve);
     return new BaseAuto(); // TODO place holder for now, replace once we have auto modes
   }
-  /* End Subsystems */
 
-  /* Button Bindings - Actions taken upon button press or hold */
-  private void configureButtonBindings() {
-    /* Driver Buttons */
-    zeroGyro.onTrue(new InstantCommand(() -> s_Swerve.zeroGyro()));
-    getGyroReading.onTrue(new InstantCommand(() -> s_Swerve.GetGyroReading()));
-    robotCentric.onTrue(new InstantCommand(() -> s_Swerve.SetRobotCentric()));
-    FieldDrive.onTrue(new InstantCommand(() -> s_Swerve.SetFieldDrive()));
-  }
 }
